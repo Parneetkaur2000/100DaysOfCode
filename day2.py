89421423ae2832d4cca2eed3806fbd1796698452
Parneetkaur2000 <62416642+Parneetkaur2000@users.noreply.github.com>
2020-07-28 11:51:39 +0530

    Three Number Sum

--- day2.py
+++ day2.py
@@ -1,18 +1,9 @@
 Three Number Sum
-Write a function that takes in a non empty array of distinct integers and an integer representing a target sum. 
-The function should find a list of triplets in the array that sum upto the target sum and return a two-dimensional array of all the these triplets.
-The numbers in each triplet should be order in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.
 
-If no three numbers sum up to the target sum, the function should return an empty array.
-
-Sample Input:
-
-array = [12, 2,1, 3, -6, 5, -8, 6]
-targetSum = 0
-
-Sample Output:
-
-[[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+Write a function that takes in a non empty array of distinct integers and an integer representing a target sum. The function should find a list of triplets in
+the array that sum upto the target sum and return a two-dimensional array of all the these triplets.The numbers in each triplet should be order in ascending 
+order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.If no three numbers sum up to the target sum, 
+the function should return an empty array
 
 *********
 def threeNumberSum(array, targetSum):
