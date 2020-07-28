f8e920716a224fb1dbae5ef53d94f530c847d6c6
Parneetkaur2000 <62416642+Parneetkaur2000@users.noreply.github.com>
2020-07-28 11:50:10 +0530

    Smallest Difference

--- day3.py
+++ day3.py
@@ -1,22 +1,8 @@
 Smallest Difference   
-Write a function that takes in two non-empty arrays of integers, find the pair of numbers (one from each array) whose absolute difference is closet to zero, and returns an array containing these two numbers, with the numbsers, with the number from the first array in the first position.
 
-You can assume that there will only be one pair of numbers with the smallest difference.
-
-Sample Input
-
-arrayOne = [-1, 5, 10, 20, 28, 3]
-arrayTwo = [26, 134, 135, 15, 17]
-
-Sample Output
-
-[28, 26]
-
-Optimal Solution
-O(nlog(n)) + O(mlog(m) + O(m+n))
-where,
-n is the size of arrayOne
-m is the size of arrayTwo
+Write a function that takes in two non-empty arrays of integers, find the pair of numbers (one from each array) whose absolute difference is closet to zero, 
+and returns an array containing these two numbers, with the numbsers, with the number from the first array in the first position.You can assume that there 
+will only be one pair of numbers with the smallest difference.
 
 *******************
 import math
